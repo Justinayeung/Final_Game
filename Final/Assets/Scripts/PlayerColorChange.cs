b21831286e0242dc460d9d615aaa1d1a38c18bcc
Justinayeung <yeunj511@newschool.edu>
2020-04-02 13:09:32 -0400

    Color Change script

---
+++ Final/Assets/Scripts/PlayerColorChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColorChange : MonoBehaviour
+{
+    Renderer rend;
+
+    private void Start() {
+        rend = GetComponent<Renderer>();
+    }
+
+    public void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Mat_Black")) { //If player is on the tag Mat_Black change player to white
+            rend.material.SetColor("_Color", Color.white);
+        }
+        if (other.CompareTag("Mat_White")) { //If player is on the tag Mat_White change player to black
+            rend.material.SetColor("_Color", Color.black);
+        }
+    }
+}
