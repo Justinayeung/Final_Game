9c9d6145a619ac326e7815aa904cf8d520634277
Justinayeung <yeunj511@newschool.edu>
2020-04-02 13:44:16 -0400

    Looping script

---
+++ Final/Assets/Scripts/LoopScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopScript : MonoBehaviour
+{
+    public Transform fallPos;
+
+    public void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Loop_Col")) {
+            transform.position = fallPos.position;
+        }
+    }
+}
