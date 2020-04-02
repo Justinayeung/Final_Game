5e5929db487f60ab83c9a33f803a18e2b1824826
jib77907 <leekp733@newschool.edu>
2020-04-01 23:43:21 -0400

    Lens distortion script base

---
+++ Final/Assets/Scripts/lensDistortion.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.PostProcessing;
+
+public class lensDistortion : MonoBehaviour
+{
+    //When I did this for the prototype I put it on the gameobject with postprocess volume on it
+
+    PostProcessVolume v;
+
+    LensDistortion lensD;
+
+    public bool distort;
+
+    void Start()
+    {
+        v = GetComponent<PostProcessVolume>();
+        v.profile.TryGetSettings<LensDistortion>(out lensD);
+        lensD.intensity.value = 0;
+
+        distort = false;
+    }
+
+
+    void Update()
+    {
+        //when a new loop starts --> distort == true
+        if (distort)
+        {
+            // this setting distorts into a repeating circle thing when the value changes a lot
+            lensD.intensity.value += 1; 
+            lensD.scale.value -= 0.01f;
+
+            distort = false;
+        }
+
+    }
+}
