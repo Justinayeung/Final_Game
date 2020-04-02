cf6bf81cb89544f58898d911fefafa4df7cb8f10
songy696 <ysong.yjs@gmail.com>
2020-04-02 15:00:07 -0400

    force on prefab

---
+++ Final/Assets/Scripts/PrefabScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabScript : MonoBehaviour
+{
+    public float thrust;
+    public float thrust2;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(0, thrust2, thrust, ForceMode.Impulse);
+    }
+}
