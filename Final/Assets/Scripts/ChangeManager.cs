aac8b6411246eb2363980f0b54c3079f510f77c1
Justinayeung <yeunj511@newschool.edu>
2020-04-04 18:42:45 -0400

    Environment changes as you loop

---
+++ Final/Assets/Scripts/ChangeManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeManager : MonoBehaviour
+{
+    LoopScript loop;
+    public List<GameObject> _OriginalObjs;
+    public List<GameObject> _ChangeObjs;
+
+    // Start is called before the first frame update
+    void Start() {
+        loop = FindObjectOfType<LoopScript>(); //Find LoopScript reference
+        for (int i = 0; i < _ChangeObjs.Count; i++) { //Set all object in _ChangeObjs to false
+            _ChangeObjs[i].SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        LoopChanges(loop.loopNum);
+    }
+
+    /// <summary>
+    /// Will set _OrginalObjs false and _ChangeObjs true base on the loop number
+    /// </summary>
+    /// <param name="loopNumber"></param>
+    public void LoopChanges(int loopNumber) {
+        if (loop.firstLoop == false) { //Do everything only if the player has gone through the loop first
+            _ChangeObjs[loopNumber - 1].SetActive(true);
+            _OriginalObjs[loopNumber - 1].SetActive(false);
+        }
+    }
+}
--- Final/Assets/Scripts/LoopScript.cs
+++ Final/Assets/Scripts/LoopScript.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class LoopScript : MonoBehaviour
 {
     public Transform fallPos;
+    public int loopNum;
+    public bool firstLoop;
+
+    void Start() {
+        loopNum = 0;
+        firstLoop = true;
+    }
 
     public void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Loop_Col")) {
+        if (other.CompareTag("Loop_Col")) { //If player his the trigger for looping
+            firstLoop = false;
             transform.position = fallPos.position;
+            loopNum++;
         }
     }
 }
