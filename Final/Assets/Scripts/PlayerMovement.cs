4d7ab1903b082eb61f5c272a5a4dd9170c824440
Nicole <polidorenn@gmail.com>
2020-04-05 14:07:36 -0400

    walking sound

--- Final/Assets/Scripts/PlayerMovement.cs
+++ Final/Assets/Scripts/PlayerMovement.cs
@@ -7,9 +7,13 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     Vector3 movement;
     Rigidbody rb;
+
+    public AudioSource audio;
+    public AudioClip walkSound;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        InvokeRepeating("PlaySound", 0.0f, 0.5f);
     }
 
     // Update is called once per frame
@@ -21,8 +25,15 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 movement = new Vector3(moveHorizontal * speed, rb.velocity.y, moveVertical * speed);
         rb.velocity = movement;
-       
+ 
+    }
 
-
+    void PlaySound()
+    {
+        if(Input.GetButton("Vertical")|| Input.GetButton("Horizontal"))
+        {
+           audio.pitch = Random.Range(.85f, 1.15f);
+            audio.PlayOneShot(walkSound);
+        }
     }
 }
