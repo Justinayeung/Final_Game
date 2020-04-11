1c8a00fd24dcd818a8205045f966baaea90af930
Justinayeung <yeunj511@newschool.edu>
2020-04-11 16:30:06 -0400

    Shadow Monster

---
+++ Final/Assets/Scripts/MonsterController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterController : MonoBehaviour
+{
+    public bool spawned;
+    public NavMeshAgent agent;
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawned) {
+            agent.SetDestination(player.position);
+        }
+    }
+}
