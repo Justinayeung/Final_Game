bdb5bc44c474c9502002bcd71e5028c520334ee3
Justinayeung <yeunj511@newschool.edu>
2020-04-13 14:25:35 -0400

    maghall

---
+++ Final/Assets/Scripts/MonsterController1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterController1 : MonoBehaviour
+{
+    public bool spawned;
+    public NavMeshAgent agent;
+    public Transform player;
+    public GameObject monster;
+    EnterMagHall mHall;
+
+    // Start is called before the first frame update
+    void Start() {
+        monster.SetActive(false);
+        mHall = FindObjectOfType<EnterMagHall>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (mHall.MagHallenabled || spawned) { //If player entered the mag hall, agent will start moving towards player
+            monster.SetActive(true);
+            agent.SetDestination(player.position);
+        }
+    }     
+}
