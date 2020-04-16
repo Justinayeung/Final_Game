a709847260b86d0a6f7151a5774a2b02b79945d3
songy696 <ysong.yjs@gmail.com>
2020-04-16 15:47:08 -0400

    Camera Rotate

--- Final/Assets/Scripts/LoopScript.cs
+++ Final/Assets/Scripts/LoopScript.cs
@@ -1,86 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class LoopScript : MonoBehaviour
 {
     public Transform fallPos;
-    public Transform fallPosInverted;
 
     public int loopNum;
     public bool firstLoop;
 
     public GameObject cam;
-    //Cinemachine.CinemachineVirtualCamera vcam;
+    CinemachineCameraOffset camOff;
 
-    public GameObject[] collectives;
-    //public bool rotateCam;
-    private bool invertedGravity;
-    //private Rigidbody rb;
+    public bool rotateCam;
 
     void Start() {
         loopNum = 0;
         firstLoop = true;
         loopIsTrue = false;
-        //rotateCam = false;
-        invertedGravity = false;
-        //rb = GetComponent<Rigidbody>();
-        //vcam = cam.GetComponent<Cinemachine.CinemachineVirtualCamera>();
+        rotateCam = false;
+        camOff = cam.GetComponent<CinemachineCameraOffset>();
     }
 
     public bool loopIsTrue;
-    public vignette vScript;
-    public lensDistortion ldScript;
+    //public vignette vScript;
+    //public lensDistortion ldScript;
 
     void Update()
     {
-        //Debug.Log("looping is" + loopIsTrue);
 
         if (loopIsTrue) {
-            vScript.increaseVignette = true;
-            ldScript.distort = true;
+            //vScript.increaseVignette = true;
+            //ldScript.distort = true;
             loopIsTrue = false;
         }
-        /*
-        if (rotateCam) {
-            //rotate the camera on Z-axis 180 degrees
-            Vector3 rotateZ = new Vector3(cam.transform.rotation.x, cam.transform.rotation.y, cam.transform.rotation.z + 180);
-            cam.transform.Rotate(rotateZ, 20.0f * Time.deltaTime);
-            //vcam.
-        }
-        */
 
-        if (invertedGravity)
+        if (rotateCam)
         {
-            Physics.gravity = Vector3.up * 9.81f * 2;
+            //rotate the camera on Z-axis 180 degrees           
+            cam.transform.rotation = Quaternion.Euler(12.399f, -9.892f, 180);
+            camOff.m_Offset = new Vector3(-3.97f, -0.27f, 0);
         }
         else {
-            Physics.gravity = Vector3.down * 9.81f * 2;
+            cam.transform.rotation = Quaternion.Euler(12.399f, 9.892f, 0);
+            camOff.m_Offset = new Vector3(4.02f, 0.31f, 0);
         }
     }
 
     public void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Loop_Col")) { //If player hits the trigger for looping
             firstLoop = false;
-            transform.position = fallPosInverted.position;
             loopNum++;
-            loopIsTrue = true;
-            collectives[3].SetActive(true);
-            collectives[4].SetActive(true);
-            collectives[5].SetActive(true);
-
-            //rotateCam = true;
-            //invert the color of the whole world
-            invertedGravity = true;
-        }
-
-        if (other.CompareTag("Loop_Col_Inverted")) {
-            //loopIsTrue = true;
             transform.position = fallPos.position;
-            invertedGravity = false;
-            collectives[0].SetActive(true);
-            collectives[1].SetActive(true);
-            collectives[2].SetActive(true);
+            rotateCam = !rotateCam;
         }
     }
 
