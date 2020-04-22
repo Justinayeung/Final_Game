447a247f3db531c8a7986d4038666dc54ec33f92
jib77907 <leekp733@newschool.edu>
2020-04-22 16:57:03 -0400

    blob room added

---
+++ Final/Assets/Scripts/blob_follow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class blob_follow : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform player;
+    LoopScript loop;
+    Animator anim;
+
+    void Start()
+    {
+        loop = FindObjectOfType<LoopScript>();
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Debug.Log(loop.loopNum);
+
+        if (loop.loopNum > 1)
+        { 
+            agent.SetDestination(player.position);
+            anim.SetBool("follow", true);
+        }
+    }
+}
