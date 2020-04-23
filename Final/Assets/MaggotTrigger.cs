e1cbeb04342509928e5c203690c66742a911f814
songy696 <ysong.yjs@gmail.com>
2020-04-23 13:42:22 -0400

    sounds + maggot fixed + ceiling for the room

--- Final/Assets/MaggotTrigger.cs
+++ Final/Assets/MaggotTrigger.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class MaggotTrigger : MonoBehaviour
 {
-    public GameObject maggots;
+    public GameObject[] maggots;
+    public LoopScript loop;
     public void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player")) {
-            maggots.SetActive(true);
+        if (other.gameObject.CompareTag("Player") && loop.loopNum == 0)
+        {
+            maggots[0].SetActive(true);
+            
+        }
+        if (other.gameObject.CompareTag("Player") && loop.loopNum == 1)
+        {
+            maggots[0].SetActive(false);
+        }
+        if (other.gameObject.CompareTag("Player") && loop.loopNum == 2) {
+            maggots[1].SetActive(true);
+            maggots[0].SetActive(false);
+            Destroy(gameObject);
         }
     }
 }
