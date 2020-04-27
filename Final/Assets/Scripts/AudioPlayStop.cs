95f21c1911413376f75f20bddaba5b5e66dfcac1
Justinayeung <yeunj511@newschool.edu>
2020-04-27 15:20:35 -0400

    camera fixes, button fixes, adding audio for button

---
+++ Final/Assets/Scripts/AudioPlayStop.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayStop : MonoBehaviour
+{
+    public AudioSource Door;
+
+    public void AudioPlay() {
+        Door.Play();
+    }
+
+    public void AudioStop() {
+        Door.Stop();
+    }
+}
--- Final/Assets/Scripts/FluidSpawn.cs
+++ Final/Assets/Scripts/FluidSpawn.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FluidSpawn : MonoBehaviour
 {
     public GameObject FluidSpawner;
+    public AudioSource Button;
 
     private void Start() {
         FluidSpawner.SetActive(false);
@@ -13,6 +14,7 @@ public class FluidSpawn : MonoBehaviour
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player")) {
             FluidSpawner.SetActive(true);
+            Button.Play();
             this.gameObject.SetActive(false);
         }
     }
