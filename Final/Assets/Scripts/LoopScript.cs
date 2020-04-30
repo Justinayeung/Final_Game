9fe18e51550592ecc23bf891cac6be396fac1aea
songy696 <ysong.yjs@gmail.com>
2020-04-30 15:48:57 -0400

    ending credit

--- Final/Assets/Scripts/LoopScript.cs
+++ Final/Assets/Scripts/LoopScript.cs
@@ -25,18 +25,27 @@ public class LoopScript : MonoBehaviour
     public Animator transition;
     public Image fadeIm;
 
+    public GameObject loopCol;
+    public GameObject endingCol;
+    bool endingCamOffset;
+    public Animator credit;
+
     void Start() {
         loopNum = 0;
         firstLoop = true;
         loopIsTrue = false;
         rotateCam = false;
         camOff = cam.GetComponent<CinemachineCameraOffset>();
+        loopCol.SetActive(true);
+        endingCol.SetActive(false);
+        endingCamOffset = false;
     }
 
     void Update()
     {
 
-        if (loopIsTrue) {
+        if (loopIsTrue)
+        {
             vScript.increaseVignette = true;
             ldScript.distort = true;
             loopIsTrue = false;
@@ -50,20 +59,34 @@ public class LoopScript : MonoBehaviour
             camOff.m_Offset = new Vector3(-2.3f, -3f, 1f);
             Physics.gravity = new Vector3(0, 9.81F, 0);
         }
-        else {
+        else
+        {
             cam.transform.rotation = Quaternion.Euler(12f, 1f, 0);
             camOff.m_Offset = new Vector3(2.3f, 1f, 1f);
             Physics.gravity = new Vector3(0, -9.81F, 0);
         }
-
+        /*
         if (loopNum >= 14)
         {
             StartCoroutine(LoadingScene(0));
-            Physics.gravity = new Vector3(0, -0.1F, 0);
+        }
+        */
+        if (loopNum == 13)
+        {
+            loopCol.SetActive(false);
+            endingCol.SetActive(true);
+        }
+        if (endingCamOffset)
+        {
+            Camera.main.backgroundColor = Color.black;
+            Physics.gravity = new Vector3(0, -0F, 0);
+            camOff.m_Offset = new Vector3(5.49f, 1f, 1f);
+            credit.SetTrigger("CreditStart");
+            StartCoroutine(LoadingScene(0));
         }
     }
 
-    public void OnTriggerEnter(Collider other) {
+        public void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Loop_Col")) { //If player hits the trigger for looping
             rotateCam = true;
             firstLoop = false;
@@ -79,11 +102,16 @@ public class LoopScript : MonoBehaviour
             loopIsTrue = true;
             loopNum++;
         }
+        if (other.CompareTag("EndingCollider")) {
+            endingCamOffset = true;
+        }
     }
 
     IEnumerator LoadingScene(int levelIndex)
     {
+        yield return new WaitForSeconds(20f);
         transition.SetTrigger("Start");
+        Physics.gravity = new Vector3(0, -9.81F, 0);
         yield return new WaitUntil(() => fadeIm.color.a == 1); //Wait until image is fully black/can be seen
         SceneManager.LoadScene(levelIndex);
     }
