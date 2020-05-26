880fdc423940630e9401668f7d145fe775613903
Nicole <polidorenn@gmail.com>
2020-05-26 11:06:28 -0400

    Merge branch 'master' of https://github.com/Justinayeung/Final_Game
    # Conflicts:
    #	Final/Assets/Scenes/Main 1.unity
    #	Final/Assets/Scenes/Menu.unity

---
+++ Final/Assets/CubeScript_Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript_Test : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float torqueAmt = 50f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        rb.AddTorque(-Vector3.forward * moveHorizontal * torqueAmt * Time.deltaTime, ForceMode.VelocityChange);
+        rb.AddTorque(Vector3.right * moveVertical * torqueAmt * Time.deltaTime, ForceMode.VelocityChange);
+    }
+}
