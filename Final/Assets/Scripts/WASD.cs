53831f50f20fe45b6f77957e5c11c9f239126724
Justinayeung <yeunj511@newschool.edu>
2020-05-28 15:38:59 -0400

    WASD script, collider, menu

--- Final/Assets/Scripts/WASD.cs
+++ Final/Assets/Scripts/WASD.cs
@@ -5,24 +5,20 @@ using UnityEngine.UI;
 
 public class WASD : MonoBehaviour
 {
-    public Image fadeIm; //Image for black screen fade
     public Animator WASDkeys;
-    bool newPlayer;
 
-    private void Awake()
-    {
-        newPlayer = true;
+    private void Awake() {
+        WASDkeys.SetBool("Shown", true);
     }
 
-    void Update()
-    {
-        if (fadeIm.color.a == 0 && newPlayer) {
-            WASDkeys.SetBool("Shown", true);
-            newPlayer = false;
-        }
-
+    void Update() {
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D)) {
-            WASDkeys.SetBool("Shown", false);
+            StartCoroutine(FadeOut());
         }
+    }
+
+    IEnumerator FadeOut() {
+        yield return new WaitForSeconds(1f);
+        WASDkeys.SetBool("Shown", false);
     }
 }
